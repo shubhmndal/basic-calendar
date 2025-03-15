ed634aa9787082846a03e3de128b73ca1e5fb54b
Shubh Mandal <shubhmandal007@gmail.com>
2025-03-15 21:15:42 +0530

    Add files via upload

---
+++ calender 2025basic.py
@@ -0,0 +1,65 @@
+import calendar
+yy = 2025
+mm = 1
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 2
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 3
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 4
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 5
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 6
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 7
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 8
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 9
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 10
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 11
+print(calendar.month(yy, mm))
+
+import calendar
+yy = 2025
+mm = 12
+print(calendar.month(yy, mm))
+
+
+
+
+
+
